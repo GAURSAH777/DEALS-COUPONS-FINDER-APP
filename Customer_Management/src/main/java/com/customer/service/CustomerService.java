62a28642818e496f9b66ac89468e3b511c185736
gaursah <gaursah@inlen8520026405.corp.capgemini.com>
2022-07-20 12:45:34 +0530

    added CustomerService.java

---
+++ Customer_Management/src/main/java/com/customer/service/CustomerService.java
@@ -0,0 +1,21 @@
+package com.customer.service;
+
+import java.util.List;
+
+import com.customer.model.Customer;
+
+public interface CustomerService {
+
+	Customer addCustomer(Customer customer);
+
+	Customer customerLogin(String username, String password);
+
+	List<Customer> getAllCustomers();
+
+	Customer getCustomerById(String customerId);
+
+	Customer updateCustomer(Customer customer, String customerId);
+
+	String deleteCustomer(String customerId);
+
+}

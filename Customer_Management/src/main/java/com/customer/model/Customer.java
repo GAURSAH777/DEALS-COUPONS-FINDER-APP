1bdaf4b8ee1c796814386fb7afdafdcd2952e653
gaursah <gaursah@inlen8520026405.corp.capgemini.com>
2022-07-21 10:06:42 +0530

    added validation in Customer.java and validation dependency in pom.xml

--- Customer_Management/src/main/java/com/customer/model/Customer.java
+++ Customer_Management/src/main/java/com/customer/model/Customer.java
@@ -1,5 +1,10 @@
 package com.customer.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,24 +13,42 @@ public class Customer {
 	@Id
 	private String customerId;
 
+	@NotNull(message = "First name should not be empty")
+	@Size(min = 2, message = "First name should not be less than 2 characters")
 	private String firstName;
 
+	@NotNull(message = "Last name should not be empty")
+	@Size(min = 2, message = "Last name should not be less than 2 characters")
 	private String lastName;
 
+	@Pattern(regexp = "\\d{10}", message = "Invalid contact details")
 	private String contactNumber;
 
+	@NotNull(message = "Email should not be empty")
+	@Email(message = "Invalid E-mail Id")
 	private String email;
 
+	@NotNull(message = "Username should not be empty")
+	@Size(min = 2, message = "Username should not be less than 2 characters")
 	private String username;
 
+	@NotNull(message = "Password should not be empty")
+	@Pattern(regexp = " ^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$", message = "Password must contain at least one digit [0-9],at least one lowercase Latin character [a-z],"
+			+ "at least one uppercase Latin character [A-Z],at least one special character like ! @ # & ( ),"
+			+ " a length of at least 8 characters and a maximum of 20 characters")
 	private String password;
 
 	public Customer() {
 
 	}
 
-	public Customer(String customerId, String firstName, String lastName, String contactNumber, String email,
-			String username, String password) {
+	public Customer(String customerId,
+			@NotNull(message = "First name should not be empty") @Size(min = 2, message = "First name should not be less than 2 characters") String firstName,
+			@NotNull(message = "Last name should not be empty") @Size(min = 2, message = "Last name should not be less than 2 characters") String lastName,
+			@Pattern(regexp = "\\d{10}", message = "Invalid contact details") String contactNumber,
+			@NotNull(message = "Email should not be empty") @Email(message = "Invalid E-mail Id") String email,
+			@NotNull(message = "Username should not be empty") @Size(min = 2, message = "Username should not be less than 2 characters") String username,
+			@NotNull(message = "Password should not be empty") @Pattern(regexp = " ^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$", message = "Password must contain at least one digit [0-9],at least one lowercase Latin character [a-z],at least one uppercase Latin character [A-Z],at least one special character like ! @ # & ( ), a length of at least 8 characters and a maximum of 20 characters") String password) {
 		super();
 		this.customerId = customerId;
 		this.firstName = firstName;

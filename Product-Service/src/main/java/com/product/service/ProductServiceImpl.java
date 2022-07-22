fa948c2a0479a7052e1dfdb7dc3778c915fac35f
gaursah <gaursah@inlen8520026405.corp.capgemini.com>
2022-07-22 12:14:32 +0530

    added ProductServiceImpl.java

---
+++ Product-Service/src/main/java/com/product/service/ProductServiceImpl.java
@@ -0,0 +1,79 @@
+package com.product.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.product.exception.ProductAlreadyExistException;
+import com.product.exception.ProductNotFoundException;
+import com.product.model.Product;
+import com.product.repository.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Override
+	public Product addProduct(Product product) {
+		boolean isProductExist = productRepository.existsById(product.getProductId());
+		if (isProductExist) {
+			throw new ProductAlreadyExistException("Product already found with this ID:" + product.getProductId());
+		} else {
+			Product savedProduct = productRepository.save(product);
+			return savedProduct;
+		}
+	}
+
+	@Override
+	public List<Product> getAllProduct() {
+		List<Product> products = productRepository.findAll();
+		if (products == null) {
+			throw new ProductNotFoundException("Products not found");
+		} else {
+			return products;
+		}
+
+	}
+
+	@Override
+	public Product getProductById(String productId) {
+		Optional<Product> optionalProduct = productRepository.findById(productId);
+		if (optionalProduct.isEmpty()) {
+			throw new ProductNotFoundException("Product not found with Id: " + productId);
+		} else {
+			Product product = optionalProduct.get();
+			return product;
+		}
+
+	}
+
+	@Override
+	public Product updateProduct(Product product) {
+		Optional<Product> optionalProduct = productRepository.findById(product.getProductId());
+		if (optionalProduct == null) {
+			throw new ProductNotFoundException("Product not Found");
+		} else {
+			Product updatedProduct = productRepository.save(product);
+			return updatedProduct;
+		}
+
+	}
+
+	@Override
+	public void deleteProductById(String productId) {
+		Optional<Product> optionalProduct = productRepository.findById(productId);
+		if (optionalProduct.isPresent()) {
+			Product product = optionalProduct.get();
+			productRepository.delete(product);
+
+		} else {
+			throw new ProductNotFoundException("Product not found with id: " + productId);
+		}
+
+	}
+
+}

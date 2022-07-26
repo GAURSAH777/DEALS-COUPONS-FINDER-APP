85a9641866f6c78641d41b273757b85fa0162d95
gaursah <gaursah@inlen8520026405.corp.capgemini.com>
2022-07-26 17:03:04 +0530

    added LOGGER

--- Cart-Service/src/main/java/com/cart/controller/CartController.java
+++ Cart-Service/src/main/java/com/cart/controller/CartController.java
@@ -2,6 +2,8 @@ package com.cart.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,24 +30,35 @@ public class CartController {
 	@Autowired
 	private CartService cartService;
 
+	private Logger LOGGER = LoggerFactory.getLogger(CartController.class);
+
 	@PostMapping("/add")
 	public ResponseEntity<Cart> addCart(@RequestBody Cart cart) {
+		LOGGER.info("Inside addCart of cartController");
 		Cart newCart = cartService.addCart(cart);
+		LOGGER.info("Adding...");
 		ResponseEntity<Cart> responseEntity = new ResponseEntity<Cart>(newCart, HttpStatus.CREATED);
+		LOGGER.info("Added cart");
 		return responseEntity;
 	}
 
 	@GetMapping("/getAll")
 	public ResponseEntity<List<Cart>> fetchAllCarts() {
+		LOGGER.info("Inside fetchAllCarts of cartController");
 		List<Cart> allCarts = cartService.getAllCarts();
+		LOGGER.info("Fetching...");
 		ResponseEntity<List<Cart>> responseEntity = new ResponseEntity<List<Cart>>(allCarts, HttpStatus.ACCEPTED);
+		LOGGER.info("Fetch Cart -END!");
 		return responseEntity;
 	}
 
 	@DeleteMapping("/delete/{id}")
 	public ResponseEntity<String> deleteProductById(@PathVariable("id") int cartId) {
+		LOGGER.info("Inside deleteProductById of cartController");
 		cartService.deleteCartById(cartId);
+		LOGGER.info("Deleting...");
 		ResponseEntity<String> responseEntity = new ResponseEntity<String>("Cart deleted successfully", HttpStatus.OK);
+		LOGGER.info("Delete Cart -END!");
 		return responseEntity;
 	}
 
--- Cart-Service/src/main/java/com/cart/service/CartServiceImpl.java
+++ Cart-Service/src/main/java/com/cart/service/CartServiceImpl.java
@@ -3,6 +3,8 @@ package com.cart.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,31 +19,47 @@ public class CartServiceImpl implements CartService {
 	@Autowired
 	private CartRepository cartRepository;
 
+	private Logger LOGGER = LoggerFactory.getLogger(CartServiceImpl.class);
+
 	@Override
 	public Cart addCart(Cart cart) {
+		LOGGER.info("Add Cart -START!");
 		Optional<Cart> savedCart = cartRepository.findById(cart.getCartId());
 		if (savedCart.isPresent()) {
+			LOGGER.info("Product Already Exist");
 			throw new ProductAlreadyExistException();
 		} else
-			return cartRepository.save(cart);
+			LOGGER.info("Saving...!!!");
+		LOGGER.info("Add Cart -END!");
+		return cartRepository.save(cart);
 	}
 
 	@Override
 	public void deleteCartById(int cartId) {
+		LOGGER.info("Delete Cart -START!");
 		Optional<Cart> optionalCart = cartRepository.findById(cartId);
 		if (optionalCart == null) {
+			LOGGER.info("Product not found");
 			throw new ProductNotFoundException();
+		} else {
+			LOGGER.info("Deleting Cart");
+			cartRepository.deleteById(cartId);
+			LOGGER.info("Delete Cart -END!");
 		}
 
 	}
 
 	@Override
 	public List<Cart> getAllCarts() {
+		LOGGER.info("Fetch Cart -START!");
 		List<Cart> carts = cartRepository.findAll();
 		if (carts == null) {
+			LOGGER.info("Product Not Found");
 			throw new ProductNotFoundException();
 		} else
-			return carts;
+			LOGGER.info("Fetching...");
+		LOGGER.info("Fetch Cart -END!");
+		return carts;
 	}
 
 }

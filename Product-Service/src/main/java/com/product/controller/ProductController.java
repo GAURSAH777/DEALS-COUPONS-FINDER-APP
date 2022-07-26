a1bdc9e4c35f21371e36182b99644dc4db21feca
gaursah <gaursah@inlen8520026405.corp.capgemini.com>
2022-07-26 16:52:31 +0530

    adder LOGGER

--- Product-Service/src/main/java/com/product/controller/ProductController.java
+++ Product-Service/src/main/java/com/product/controller/ProductController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,43 +33,60 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
+	private Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+
 	/*
 	 * This method is used to add new products
 	 */
 
 	@PostMapping("/add")
 	public ResponseEntity<Product> addProduct(@Valid @RequestBody Product product) {
+		LOGGER.info("Inside addProduct of ProductController");
 		Product newProduct = productService.addProduct(product);
+		LOGGER.info("Product added Successfully");
 		ResponseEntity<Product> responseEntity = new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+		LOGGER.info("Add product -END!");
 		return responseEntity;
 	}
 
 	@GetMapping("/all")
 	public ResponseEntity<List<Product>> getAllProducts() {
+		LOGGER.info("Inside getAllProducts of ProductController");
 		List<Product> allProducts = productService.getAllProduct();
+		LOGGER.info("Product viewed Successfully");
 		ResponseEntity<List<Product>> responseEntity = new ResponseEntity<List<Product>>(allProducts, HttpStatus.FOUND);
+		LOGGER.info("Get Product -End");
 		return responseEntity;
 	}
 
 	@GetMapping("/product/{id}")
 	public ResponseEntity<Product> fetchProductById(@PathVariable("id") String productId) {
+		LOGGER.info("Inside fetchProductById of ProductController");
 		Product product = productService.getProductById(productId);
+		LOGGER.info("Fetching Product");
 		ResponseEntity<Product> responseEntity = new ResponseEntity<Product>(product, HttpStatus.FOUND);
+		LOGGER.info("Fetch Product -END!");
 		return responseEntity;
 	}
 
 	@PutMapping("/update")
 	public ResponseEntity<Product> updateProduct(@Valid @RequestBody Product product) {
+		LOGGER.info("Inside updateProduct of ProductController");
 		Product updatedProduct = productService.updateProduct(product);
+		LOGGER.info("Updating Product");
 		ResponseEntity<Product> responseEntity = new ResponseEntity<Product>(updatedProduct, HttpStatus.CREATED);
+		LOGGER.info("Update Product -END!");
 		return responseEntity;
 	}
 
 	@DeleteMapping("/delete/{id}")
 	public ResponseEntity<String> deleteProductById(@PathVariable("id") String productId) {
+		LOGGER.info("Inside deleteProductById of ProductController");
 		productService.deleteProductById(productId);
+		LOGGER.info("Deleting Product");
 		ResponseEntity<String> responseEntity = new ResponseEntity<String>("Product deleted successfully",
 				HttpStatus.OK);
+		LOGGER.info("Delete Product -END!");
 		return responseEntity;
 	}
 
--- Product-Service/src/main/java/com/product/service/ProductServiceImpl.java
+++ Product-Service/src/main/java/com/product/service/ProductServiceImpl.java
@@ -3,6 +3,8 @@ package com.product.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,24 +19,28 @@ public class ProductServiceImpl implements ProductService {
 	@Autowired
 	private ProductRepository productRepository;
 
+	private Logger LOGGER = LoggerFactory.getLogger(ProductServiceImpl.class);
+
 	@Override
 	public Product addProduct(Product product) {
-//		Optional<Product> optionalProduct = productRepository.findById(product.getProductId());
-//		if (optionalProduct.isEmpty()) {
+		LOGGER.info("Add Product-START!");
 		Product savedProduct = productRepository.save(product);
+		LOGGER.info("Product added successfully!!");
+		LOGGER.info("Add Product-END!");
 		return savedProduct;
 
-//		} else {
-//			throw new ProductAlreadyExistException("Product Already Exist with id: " + product.getProductId());
-//		}
 	}
 
 	@Override
 	public List<Product> getAllProduct() {
+		LOGGER.info("Get Product-START!");
 		List<Product> products = productRepository.findAll();
 		if (products == null) {
+			LOGGER.info("Product Not Found");
 			throw new ProductNotFoundException("Products not found");
 		} else {
+			LOGGER.info("Displaying Product!");
+			LOGGER.info("Get Product-END!");
 			return products;
 		}
 
@@ -42,11 +48,15 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public Product getProductById(String productId) {
+		LOGGER.info("Get Product-START!");
 		Optional<Product> optionalProduct = productRepository.findById(productId);
 		if (optionalProduct.isEmpty()) {
+			LOGGER.info("Product Not Found");
 			throw new ProductNotFoundException("Product not found with Id: " + productId);
 		} else {
 			Product product = optionalProduct.get();
+			LOGGER.info("Displaying Product!");
+			LOGGER.info("Add Product-END!");
 			return product;
 		}
 
@@ -54,11 +64,15 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public Product updateProduct(Product product) {
+		LOGGER.info("Update Product -START!");
 		Optional<Product> optionalProduct = productRepository.findById(product.getProductId());
 		if (optionalProduct == null) {
+			LOGGER.info("Product Not Found");
 			throw new ProductNotFoundException("Product not Found");
 		} else {
 			Product updatedProduct = productRepository.save(product);
+			LOGGER.info("Updating Product ");
+			LOGGER.info("Update Product -END!");
 			return updatedProduct;
 		}
 
@@ -66,12 +80,15 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public void deleteProductById(String productId) {
+		LOGGER.info("Delete Product -START!");
 		Optional<Product> optionalProduct = productRepository.findById(productId);
 		if (optionalProduct.isPresent()) {
 			Product product = optionalProduct.get();
+			LOGGER.info("Delete Product -End!");
 			productRepository.delete(product);
 
 		} else {
+			LOGGER.info("Product Not Found");
 			throw new ProductNotFoundException("Product not found with id: " + productId);
 		}
 

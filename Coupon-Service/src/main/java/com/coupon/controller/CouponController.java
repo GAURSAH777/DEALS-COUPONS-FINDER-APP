bb6086d7db2fa9476cfb3d01e354b61c63f3921e
gaursah <gaursah@inlen8520026405.corp.capgemini.com>
2022-07-26 15:57:48 +0530

    added LOGGER

--- Coupon-Service/src/main/java/com/coupon/controller/CouponController.java
+++ Coupon-Service/src/main/java/com/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,65 +33,78 @@ public class CouponController {
 	@Autowired
 	private CouponService couponService;
 
+	private Logger LOGGER = LoggerFactory.getLogger(CouponController.class);
+
 	/*
 	 * fetchAllCoupons() is used to get all the list of coupons
 	 */
-	
+
 	@GetMapping("/allCoupons")
 	public List<Coupon> fetchAllCoupons() {
+		LOGGER.info("Inside fetchAllCoupons of CouponController");
 
 		List<Coupon> coupons = couponService.getAllCoupons();
+		LOGGER.info("Fetching  Coupons");
+		LOGGER.info("Fetching  Coupon -END!");
 		return coupons;
 	}
-	
+
 	/*
 	 * addCoupon() is used to add coupon details
 	 */
 
 	@PostMapping("/save")
 	public ResponseEntity<Coupon> addCoupon(@Valid @RequestBody Coupon coupon) {
-
+		LOGGER.info("Inside addCoupon of CouponController");
 		Coupon newCoupon = couponService.addCoupon(coupon);
 		ResponseEntity<Coupon> responseEntity = new ResponseEntity<>(newCoupon, HttpStatus.CREATED);
+		LOGGER.info("adding  Coupons");
+		LOGGER.info("Adding  Coupon -END!");
 		return responseEntity;
 	}
-	
+
 	/*
 	 * fetchCouponById() is used to get the detail of particular coupon
 	 */
 
 	@GetMapping("/get/{id}")
 	public ResponseEntity<?> fetchCouponById(@PathVariable("id") String couponId) {
-
+		LOGGER.info("Inside fetchCouponById of CouponController");
 		ResponseEntity<?> responseEntity = null;
 		Coupon coupon = couponService.getCouponById(couponId);
 		responseEntity = new ResponseEntity<>(coupon, HttpStatus.OK);
+		LOGGER.info("Fetching  Coupons");
+		LOGGER.info("Fetching  Coupon -END!");
 		return responseEntity;
 	}
-	
+
 	/*
 	 * deleteCouponById() is used to delete/remove a particular coupon
 	 */
 
 	@DeleteMapping("/delete/{id}")
 	public ResponseEntity<String> deleteCouponById(@PathVariable("id") String couponId) {
-
+		LOGGER.info("Inside deleteCouponById of CouponController");
 		ResponseEntity<String> responseEntity = null;
 		couponService.deleteCoupon(couponId);
 		responseEntity = new ResponseEntity<>("Coupon deleted successfully", HttpStatus.OK);
+		LOGGER.info("Deleting  Coupons");
+		LOGGER.info("Deleting  Coupon -END!");
 		return responseEntity;
 	}
-	
+
 	/*
 	 * updateCoupon() is used to update a particular coupon detail
 	 */
 
 	@PutMapping("/update")
 	public ResponseEntity<Object> updateCoupon(@Valid @RequestBody Coupon coupon) {
-
+		LOGGER.info("Inside updateCoupon of CouponController");
 		ResponseEntity<Object> responseEntity = null;
 		couponService.updateCoupon(coupon);
 		responseEntity = new ResponseEntity<>("Coupon updated successfully", HttpStatus.OK);
+		LOGGER.info("Updating  Coupons");
+		LOGGER.info("Updating  Coupon -END!");
 		return responseEntity;
 	}
 
--- Coupon-Service/src/main/java/com/coupon/service/CouponServiceImpl.java
+++ Coupon-Service/src/main/java/com/coupon/service/CouponServiceImpl.java
@@ -3,6 +3,8 @@ package com.coupon.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,50 +18,71 @@ public class CouponServiceImpl implements CouponService {
 	@Autowired
 	private CouponRepository couponRepository;
 
+	private Logger LOGGER = LoggerFactory.getLogger(CouponServiceImpl.class);
+
 	@Override
 	public Coupon addCoupon(Coupon coupon) {
+		LOGGER.info("Add Coupon -START!");
 		Coupon savedCoupon = couponRepository.save(coupon);
+		LOGGER.info("Coupon added Successfully!!");
+		LOGGER.info("Add Coupon -End!");
 		return savedCoupon;
 	}
 
 	@Override
 	public List<Coupon> getAllCoupons() {
+		LOGGER.info("Get Coupon -START!");
 		List<Coupon> coupons = couponRepository.findAll();
+		LOGGER.info("Displaying Coupon!");
+		LOGGER.info("Get Coupon -End!");
 		return coupons;
 	}
 
 	@Override
 	public Coupon getCouponById(String couponId) {
+		LOGGER.info("Get Coupon -START!");
 		Optional<Coupon> optionalCoupon = couponRepository.findById(couponId);
-		if (optionalCoupon.isEmpty())
+		if (optionalCoupon.isEmpty()) {
+			LOGGER.info("Coupon Not Found!");
 			throw new CouponNotFoundException("Coupon Not found with id : " + couponId);
-		Coupon coupon = optionalCoupon.get();
-		return coupon;
+		} else {
+
+			Coupon coupon = optionalCoupon.get();
+			LOGGER.info("Displaying Coupon!");
+			LOGGER.info("Get Coupon -End!");
+			return coupon;
+		}
 	}
 
 	@Override
 	public Coupon updateCoupon(Coupon coupon) {
+		LOGGER.info("Update Coupon -START!");
 		Optional<Coupon> optionalCoupon = couponRepository.findById(coupon.getCouponId());
 
 		if (optionalCoupon == null) {
+			LOGGER.info("Coupon Not Found");
 			throw new CouponNotFoundException("Coupon not exising with id: " + coupon.getCouponId());
 		}
 
 		Coupon updatedCoupon = couponRepository.save(coupon);
-
+		LOGGER.info("Coupon updated Successfully!");
+		LOGGER.info("Update Coupon -End!");
 		return updatedCoupon;
 	}
 
 	@Override
 	public void deleteCoupon(String couponId) {
+		LOGGER.info("Delete Coupon -START!");
 		Optional<Coupon> optionalCoupon = couponRepository.findById(couponId);
 
 		if (optionalCoupon == null) {
+			LOGGER.info("Coupon Not Found");
 			throw new CouponNotFoundException("Customer not exising with id: " + couponId);
 		}
 
 		Coupon coupon = optionalCoupon.get();
-
+		LOGGER.info("Coupon deleted Successfully!");
+		LOGGER.info("Delete Coupon -End!");
 		couponRepository.delete(coupon);
 
 	}

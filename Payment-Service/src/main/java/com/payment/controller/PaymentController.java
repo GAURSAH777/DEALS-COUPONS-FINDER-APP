dc1f9d887200aab483cfe8138e812733e2cb30f1
gaursah <gaursah@inlen8520026405.corp.capgemini.com>
2022-07-26 17:10:47 +0530

    added LOGGER

--- Payment-Service/src/main/java/com/payment/controller/PaymentController.java
+++ Payment-Service/src/main/java/com/payment/controller/PaymentController.java
@@ -2,6 +2,8 @@ package com.payment.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,26 +29,36 @@ public class PaymentController {
 	@Autowired
 	private PaymentService paymentService;
 
+	private Logger LOGGER = LoggerFactory.getLogger(PaymentController.class);
+
 	@PostMapping("/save")
 	public ResponseEntity<PaymentDetails> addPayment(@RequestBody PaymentDetails payment) {
+		LOGGER.info("Inside addPayment of PaymentController");
 		PaymentDetails savedPayment = paymentService.addPayment(payment);
+		LOGGER.info("Adding...");
 		ResponseEntity<PaymentDetails> responseEntity = new ResponseEntity<PaymentDetails>(savedPayment,
 				HttpStatus.CREATED);
+		LOGGER.info("Payment Successfull");
+		LOGGER.info("Add Payment -End!");
 		return responseEntity;
 	}
 
 	@GetMapping("/getAll")
 	public ResponseEntity<List<PaymentDetails>> fetchAllPayment() {
+		LOGGER.info("Inside fetchAllPayments of PaymentController");
 		List<PaymentDetails> allPayment = paymentService.viewAllPaymentDetails();
 		ResponseEntity<List<PaymentDetails>> responseEntity = new ResponseEntity<List<PaymentDetails>>(allPayment,
 				HttpStatus.ACCEPTED);
+		LOGGER.info("Fetch Payment -End!");
 		return responseEntity;
 	}
 
 	@GetMapping("/get/{id}")
 	public ResponseEntity<PaymentDetails> fetchPaymentDetailById(@PathVariable("id") String paymentId) {
+		LOGGER.info("Inside fetchPayment of PaymentController");
 		PaymentDetails pay = paymentService.viewPaymentDetailById(paymentId);
 		ResponseEntity<PaymentDetails> responseEntity = new ResponseEntity<PaymentDetails>(pay, HttpStatus.ACCEPTED);
+		LOGGER.info("Fetch Payment -End!");
 		return responseEntity;
 	}
 
--- Payment-Service/src/main/java/com/payment/service/PaymentServiceImpl.java
+++ Payment-Service/src/main/java/com/payment/service/PaymentServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import java.util.Random;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +20,15 @@ public class PaymentServiceImpl implements PaymentService {
 	@Autowired
 	private PaymentRepository paymentRepository;
 
+	private Logger LOGGER = LoggerFactory.getLogger(PaymentServiceImpl.class);
+
 	@Override
 	public PaymentDetails addPayment(PaymentDetails payment) {
+		LOGGER.info("Add Payment -START!");
 		payment.setPaymentStatus(paystatus());
 		payment.setPaymentId(UUID.randomUUID().toString());
+		LOGGER.info("Payment Successfull!!!");
+		LOGGER.info("Add Payment -END!");
 		return paymentRepository.save(payment);
 	}
 
@@ -31,20 +38,27 @@ public class PaymentServiceImpl implements PaymentService {
 
 	@Override
 	public List<PaymentDetails> viewAllPaymentDetails() {
+		LOGGER.info("View Payment -START!");
 		List<PaymentDetails> pay = paymentRepository.findAll();
 		if (pay == null) {
+			LOGGER.info("Payment Not Found");
 			throw new PaymentNotFoundException();
 		} else
-			return pay;
+
+			LOGGER.info("View Payment -End!");
+		return pay;
 	}
 
 	@Override
 	public PaymentDetails viewPaymentDetailById(String paymentId) {
+		LOGGER.info("View Payment -START!");
 		Optional<PaymentDetails> payment = paymentRepository.findById(paymentId);
 		if (payment == null) {
+			LOGGER.info("Payment Not Found");
 			throw new PaymentNotFoundException();
 		} else {
 			PaymentDetails list = payment.get();
+			LOGGER.info("View Payment -END!");
 			return list;
 		}
 

3c64b47f0842072f3419a35e4487aadcc8a373cf
gaursah <gaursah@inlen8520026405.corp.capgemini.com>
2022-07-27 08:45:55 +0530

    updated

--- Customer_Management/src/main/java/com/customer/model/Customer.java
+++ Customer_Management/src/main/java/com/customer/model/Customer.java
@@ -45,7 +45,7 @@ public class Customer {
 
 	}
 
-	public Customer(
+	public Customer(String customerId,
 			@NotNull(message = "First name should not be empty") @Size(min = 2, message = "First name should not be less than 2 characters") String firstName,
 			@NotNull(message = "Last name should not be empty") @Size(min = 2, message = "Last name should not be less than 2 characters") String lastName,
 			@Pattern(regexp = "\\d{10}", message = "Invalid contact details") String contactNumber,
@@ -53,12 +53,17 @@ public class Customer {
 			@NotNull(message = "Username should not be empty") @Size(min = 2, message = "Username should not be less than 2 characters") String username,
 			@NotNull(message = "Password should not be empty") @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Password Should contain:\n1)a digit must occur at least once\n 2)a lower case letter must occur at least once\n\r\n 3)an upper case letter must occur at least once\n4)a special character must occur at least once\n\r\n5)no whitespace allowed in the entire string\n6)at least 8 characters") String password) {
 		super();
+		this.customerId = customerId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.contactNumber = contactNumber;
 		this.email = email;
 		this.username = username;
 		this.password = password;
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
 	}
 
 	public String getCustomerId() {
